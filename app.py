968718416d0b00b26e23e622e9cc8cec2901ffb6
YourDad07 <salmanadhikari415@gmail.com>
2021-09-11 09:10:57 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+import os
+app = Flask(__name__)
+app.config['SECRET_KEY'] ='secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.username
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/submit',methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        try:
+            passw = request.form['passw']
+            us = User(username=passw)
+            db.session().add(us)
+            db.session().commit()
+            return 'YES'
+        except:
+            return 'NO'
+    
+if(__name__)=='__main__':
+    app.run(debug=True)
